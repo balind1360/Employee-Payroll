b3abf031d32b8c5d12cc27a8054913b6c3f4408d
juliah2025 <julia_heintz@ryecountryday.org>
2024-09-12 12:15:47 -0400

    Create visual.java

---
+++ visual.java
@@ -0,0 +1,18 @@
+/* 
+Julia Heintz 
+date created: 9.12.2024
+last edited: 9.12.2024 
+
+visual/GUI components for the employee-payroll 
+- sign in screen 
+- employee view 
+  - see hours worked per date in the pay period 
+  - calculate hours worked in that pay period 
+- admin view 
+  - shut down button (x in left corner) -> are you sure? screen 
+  - view employee -> edit employee 
+*/ 
+
+import java.io.*; 
+import javax.swing.*;
+import java.awt.event.*;
