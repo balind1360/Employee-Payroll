97feedd01f7f8195f2cc5e7cd7fb7e157a6a42cf
balind1360 <141941014+balind1360@users.noreply.github.com>
2024-09-16 09:13:27 -0400

    automatic saving

--- Employee.java
+++ Employee.java
@@ -16,6 +16,7 @@ public class Employee {
         workExperience = newWorkExperience;
         hourly = newHourly;
         salary = newSalary;
+        this.createFile();
     }
 
     public Employee (String id){
