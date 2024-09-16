bf0efdf890a5ff157c0d078cdd86f893eb3daff4
juliah2025 <julia_heintz@ryecountryday.org>
2024-09-15 23:45:48 -0400

    Update welcomeVisual.java

--- visual.java
+++ visual.java
@@ -1,7 +1,7 @@
 /* 
 Julia Heintz 
 date created: 9.12.2024
-last edited: 9.12.2024 
+last edited: 9.15.2024 
 
 visual/GUI components for the employee-payroll 
 - sign in screen 
@@ -15,16 +15,50 @@ visual/GUI components for the employee-payroll
 
 import java.io.*; 
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.event.*; 
+import java.awt.*;
 
-public class MyGUI{ 
-  private static JLabel label; 
-  private static int value;
+//used geeks4geeks & oracle to learn boxlayout
+//import packages for box layout 
+import javax.swing.BoxLayout; 
+import javax.swing.Box; 
 
-  public static void main(String args[]){
-    JFrame frame = new JFrame("Employee Manager");
-    frame.setSize(
-  } 
+public class EMgui{ 
+	//label for text
+	private static JLabel label; 
 
-} 
+	public static void main(String args[]){
+		//declare & set size of initial frame
+		JFrame iFrame = new JFrame("Employee Manager");
+		iFrame.setSize(600,400);
+		//set close
+		iFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		//new panel
+		JPanel comp = new JPanel();
+		//set layout for panel (box layout for Y axis)
+		BoxLayout boxlayout = new BoxLayout(comp, BoxLayout.Y_AXIS);
+		comp.setLayout(boxlayout);
+
+		//initialize label
+		label = new JLabel("Enter Employee ID: ");
+		label.setSize(100,100);
+		
+		//added a text window for employees to type in their ID
+		JTextField textWindow = new JTextField();
+		textWindow.setHorizontalAlignment(JTextField.CENTER);
+		textWindow.setBackground(Color.PINK);
+		textWindow.setSize(100,80);
+		textWindow.setEditable(true);
+		
+
+		//add label & window to the panel 
+		comp.add(label);
+		comp.add(textWindow);
+		//add panel to the frame
+		iFrame.add(comp);
+
+		// set visible in order for frame to appear 
+		iFrame.setVisible(true);
+	}
+}
