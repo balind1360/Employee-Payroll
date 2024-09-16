de09ee58c4f7c1a17a0b6381cb79cd1d6a142bc2
juliah2025 <julia_heintz@ryecountryday.org>
2024-09-16 09:42:39 -0400

    Update welcomeVisual.java

--- welcomeVisual.java
+++ welcomeVisual.java
@@ -22,41 +22,56 @@ import java.awt.*;
 //import packages for box layout 
 import javax.swing.BoxLayout; 
 import javax.swing.Box; 
+import java.awt.Component;
+
 
 public class EMgui{ 
 	//label for text
 	private static JLabel label; 
 
 	public static void main(String args[]){
+
+		
+
 		//declare & set size of initial frame
 		JFrame iFrame = new JFrame("Employee Manager");
-		iFrame.setSize(600,400);
+		iFrame.setSize(500,300);
 		//set close
 		iFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		//new panel
+		//create parent panel
 		JPanel comp = new JPanel();
 		//set layout for panel (box layout for Y axis)
-		BoxLayout boxlayout = new BoxLayout(comp, BoxLayout.Y_AXIS);
+		comp.setPreferredSize(new Dimension(500,300));
+		BoxLayout boxlayout = new BoxLayout(comp, BoxLayout.PAGE_AXIS);
 		comp.setLayout(boxlayout);
+
+
+
+	
 
 		//initialize label
 		label = new JLabel("Enter Employee ID: ");
-		label.setSize(100,100);
+		label.setSize(200,200);
+		label.setAlignmentX(Component.CENTER_ALIGNMENT);
+		comp.add(label);
+
+		
 		
 		//added a text window for employees to type in their ID
 		JTextField textWindow = new JTextField();
 		textWindow.setHorizontalAlignment(JTextField.CENTER);
 		textWindow.setBackground(Color.PINK);
-		textWindow.setSize(100,80);
+		textWindow.setMaximumSize(new Dimension(250,40));
 		textWindow.setEditable(true);
+		comp.add(textWindow);
+
 		
 
-		//add label & window to the panel 
-		comp.add(label);
-		comp.add(textWindow);
+	
 		//add panel to the frame
 		iFrame.add(comp);
+		iFrame.pack();
 
 		// set visible in order for frame to appear 
 		iFrame.setVisible(true);
