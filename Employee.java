a3ff6cb12ff55aae311b6c0784f7928e81120698
eblock7 <ellie_block@ryecountryday.org>
2024-09-16 08:42:32 -0400

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -5,8 +5,10 @@ public class Employee {
     String workExperience;
     boolean hourly;
     double salary;
+    boolean clockedIn;
+    //hoursWorked
 
-    public employee (String newName, String newEmployeeID, String newBirthday, String newWorkExperience, boolean newHourly, double newSalary){
+    public employee (String newName, String newEmployeeID, String newBirthday, String newWorkExperience, boolean newHourly, double newSalary, boolean clockedIn){
         name = newName;
         employeeID = newEmployeeID;
         birthday = newBirthday;
@@ -15,7 +17,60 @@ public class Employee {
         salary = newSalary;
     }
 
-    public toString (){
+    public String toString (){
+        return name;
+    }
 
+    public String getEmployeeID (){
+        return employeeID;
+    }
+
+    public void setEmployeeID (String newID){
+        employeeID = newID;
+    }
+
+    public String getBirthday (){
+        return birthday;
+    }
+
+    public void setBrithday (String newBirthday){
+        birthday = newBirthday;
+    }
+
+    public String getWorkExperience (){
+        return workExperience;
+    }
+
+    public void editWorkExperience (String newWorkExperience){
+        workExperience = newWorkExperience;
+    }
+
+    public boolean getHourly (){
+        return hourly;
+    }
+
+    public void setHourly (boolean newHourly){
+        hourly = newHourly;
+    }
+
+    public double getSalary (){
+        return salary;
+    }
+
+    public void setSalary (double newSalary){
+        salary = newSalary;
+    }
+
+    public boolean getClockedIn (){
+        return clockedIn;
+    }
+
+    public void setClockedIn (boolean newClockedIn){\
+        clockedIn = newClockedIn;
+    }
+
+
+    public double calculatePay (){
+        
     }
 }
