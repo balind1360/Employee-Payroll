fdb973b04c44f77b688b42f72988d173b11d515f
juliah2025 <julia_heintz@ryecountryday.org>
2024-09-17 19:33:50 -0400

    Create JuliaNotes.java
    simply a space for Julia to talk to herself and keep notes

---
+++ JuliaNotes.java
@@ -0,0 +1,10 @@
+/* 
+visual/GUI components for the employee-payroll 
+- sign in screen 
+- employee view 
+  - see hours worked per date in the pay period 
+  - calculate hours worked in that pay period 
+- admin view 
+  - shut down button (x in left corner) -> are you sure? screen 
+  - view employee -> edit employee 
+*/
