b403627c39b9ab46a1633575566a9307a8d87d3e
balind1360 <141941014+balind1360@users.noreply.github.com>
2024-09-18 14:27:08 -0400

    adding in payroll changes

--- Admin.java
+++ Admin.java
@@ -1,8 +1,20 @@
 import java.io.*;
 import java.util.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Name: Admin                               Last Updated: 11/18/24
+ * 
+ * Creator: D. Balin
+ * 
+ * Purpose: Class to make Admin objects that can find all employees, add employees, save all information changes, and get a payroll text file
+ */
+
 
 public class Admin{
 
+    // gets a list of all employees
     public static ArrayList<String> allEmployees(String path){
         File emp_fold = new File(path);  
         File[] emp_files = emp_fold.listFiles();
@@ -17,12 +29,152 @@ public class Admin{
         return list;
     }
 
-    public void save(){
+    // adds an Employee with given values, returns said Employee
+    public Employee addEmployee(String name, String id, String birthday, String experience, boolean hourly, double salary){
+        if(allEmployees("/Users/danabalin/Documents/AT2-Employee-Payroll/").contains(id)){
+            return (new Employee(name, id, birthday, experience, hourly, salary));
+        }
+        else{
+            return null;
+        }
+    }
+
+    // deletes file for employee with given id (String id)
+    public void removeEmployee(String id){
+        File file = new File("/Users/danabalin/Documents/AT2-Employee-Payroll/" + id + ".txt");
+ 
+        if (file.delete()) {
+            System.out.println("File deleted successfully");
+        }
+        else {
+            System.out.println("Failed to delete the file");
+        }
+    }
+
+    // saves all employees by parsing through the current files
+    public void saveAll(){
         ArrayList<String> emps = allEmployees("/Users/danabalin/Documents/AT2-Employee-Payroll/");
         Employee emp;
         for (String e: emps){
             emp = new Employee(e);
+            emp.clockOut();
             emp.save();
         }
+    }
+
+    // creates a file with each line being in the format (employee_id), $(pay)
+    public void getPayroll(){
+        ArrayList<String> emps = allEmployees("/Users/danabalin/Documents/AT2-Employee-Payroll/");
+        Employee emp;
+        ArrayList<String> pay = new ArrayList<String>();
+
+        // FILE READING -------------------------------------------------------------
+        
+        FileReader fr = null;
+        Scanner in = null;
+        
+        try {
+            fr = new FileReader("/Users/danabalin/Documents/AT2-Employee-Payroll/Current_Payroll.txt");
+            in = new Scanner (fr);
+        } catch (IOException e){
+            return;
+        }
+
+        LocalDate start = LocalDate.parse(in.nextLine());
+
+        ArrayList<LocalDate> holidays = new ArrayList<LocalDate>();
+
+        while (in.hasNext()){
+            holidays.add(LocalDate.parse(in.nextLine()));
+        }
+
+        try {
+            fr.close();
+            in.close();
+        } catch (IOException e){
+            return;
+        }
+
+
+        // CALCULATIONS -------------------------------------------------------------
+
+        for (String e: emps){
+            emp = new Employee(e);
+            //clocks out employees
+            emp.clockOut();
+            pay.add(e + ", $" +  emp.calculatePay());
+            // resets their pay period
+            emp.resetPayPeriod();
+            emp.save();
+        }
+
+
+        // FILE WRITING -------------------------------------------------------------
+
+        FileWriter fw = null;
+        PrintWriter out = null;
+
+        // rewrites the file consisting of all holidays and the start of the payroll to be for the newest payroll 
+        try{
+            fw = new FileWriter("/Users/danabalin/Documents/AT2-Employee-Payroll/Current_Payroll.txt", false);
+            out = new PrintWriter(fw);
+        } catch(IOException e){
+            System.out.println("Error opening output file: " + e);
+            System.exit(1);
+        }
+
+        out.println(LocalDate.now());
+
+        try{
+            fw.close();
+            out.close();
+        } catch(IOException e){
+            System.out.println("Error closing files " + e);
+        }
+
+        // writes salary of all employess to file named Newest_Payroll.txt
+        try{
+            fw = new FileWriter("/Users/danabalin/Documents/AT2-Employee-Payroll/Payroll.txt", false);
+            out = new PrintWriter(fw);
+        } catch(IOException e){
+            System.out.println("Error opening output file: " + e);
+            System.exit(1);
+        }
+
+        for(String e: pay){
+            out.println(e);
+        }
+
+        try{
+            fw.close();
+            out.close();
+        } catch(IOException e){
+            System.out.println("Error closing files " + e);
+        }
+
+    }
+
+    public void addHoliday(LocalDate day){
+        FileWriter fw = null;
+        PrintWriter out = null;
+        
+        // writes information to file
+        try{
+            fw = new FileWriter("/Users/danabalin/Documents/AT2-Employee-Payroll/Current_Payroll.txt", true);
+            out = new PrintWriter(fw);
+        } catch(IOException e){
+            System.out.println("Error opening output file: " + e);
+            System.exit(1);
+        }
+
+        out.println(day);
+
+        try{
+            fw.close();
+            out.close();
+        } catch(IOException e){
+            System.out.println("Error closing files " + e);
+        }
+
     }
 }
